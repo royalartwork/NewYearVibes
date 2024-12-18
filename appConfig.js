34611364c126c42439c1fea49f5f268d328399fb
royalartwork <royalartwork8@gmail.com>
2024-12-19 05:20:56 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  title: "JUDUL",
-  tagline: "Coloring Me Free Coloring Page",
-  description: "Coloring Me Free Coloring Pag.",
-  keywords: "coloring,coloringkids,agc,cdom blog",
-  author: "Coloringme.biz.id",
-  favicon: "https://pugjs.org/images/favicon.ico",
+  title: "New Year Vibes",
+  tagline: "Catch the New Year vibes! ✨ Inspiring ideas for celebrations, decorations, recipes, and a fresh start",
+  description: "Catch the New Year vibes! ✨ Inspiring ideas for celebrations, decorations, recipes, and a fresh start",
+  keywords: "New Years nails,New year nails simple,New year nails ideas,Christmas nails,New Years Nails,New year nails short,New Years Eve Outfit Dress to Impress,New Year Outfit Men,New year outfit ideas,New Year outfit ideas female,New Year's Eve outfit ideas,New years eve outfit dti,New year eve outfits,New Year Eve artinya,New Year's Eve,New Years Eve Party,New Years Eve events,New Year's Eve movie,New Year's Eve fireworks,New Year's Eve,New year decorations ideas,New year decorations for home,New Year's Eve party food buffet,25 best appetizers,Appetizer party menu,Appetizers to impress,Hearty appetizers,Small shrimp appetizer recipes",
+  author: "Milyader Muda",
+  favicon: "",
   imageCount: 20,
   removeBadWords: "porn,nude,sex",
 };
