85959c037e9f5ba1ac3416d881407422fceb2368
royalartwork <royalartwork8@gmail.com>
2024-12-19 05:19:59 +0700

    Add files via upload

---
+++ appConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "JUDUL",
+  tagline: "Coloring Me Free Coloring Page",
+  description: "Coloring Me Free Coloring Pag.",
+  keywords: "coloring,coloringkids,agc,cdom blog",
+  author: "Coloringme.biz.id",
+  favicon: "https://pugjs.org/images/favicon.ico",
+  imageCount: 20,
+  removeBadWords: "porn,nude,sex",
+};
